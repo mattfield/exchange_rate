7dee544ecb18b3194b8485b64a7dfb27261649e8
Matt Field <m.field42@gmail.com>
2017-01-12 16:35:55 +0000

    Returns rates for requested date range only 	modified:   lib/exchange_rate/quote.rb

--- lib/exchange_rate/quote.rb
+++ lib/exchange_rate/quote.rb
@@ -3,24 +3,47 @@ require 'exchange_rate/helper'
 
 class Quote
   DEFAULT_AMOUNT = 1
-  DEFAULT_BASE = 'EUR'
+  DEFAULT_FROM = 'EUR'
+
+  def initialize(options = {})
+    @date = options['date']
+    @amount = options['amount'] || DEFAULT_AMOUNT
+    @from = options['from'] || DEFAULT_FROM
+    @to = options['to']
+    rates
+  end
+
+  attr_reader :amount, :date, :from, :to
 
   def rates
     @rates ||= get_rates
   end
 
-  def get_rates
-    ECBFeed.new().each do |rate|
-      rate
-    end
+  def date=(value)
+    @date = value ? date_in_range?(value) : @date
   end
 
-  def get_rates_by_date date
-    # Make sure any dates coming in get converted to
-    # xmlschema standard i.e. YYYY-MM-DD
-    date = Helper::ensure_weekday(Date.parse(date).xmlschema)
+  def amount=(value)
+    @amount = (value || DEFAULT_AMOUNT).to_f
+    raise StandardError, 'Invalid amount - must be greater than zero' if @amount.zero?
+  end
 
-    REXML::XPath.each(get_rates, "[@time='#{date}']") do |rate|
+  def from=(value)
+    @from = value.upcase || DEFAULT_FROM
+  end
+
+  def get_rates
+    rates = using_default_from? ? get_default_rates : get_updated_rates
+  end
+
+  def get_default_rates
+    new_rates = ECBFeed.new().each do |rate|
+      rate
+    end
+
+    date = ensure_weekday @date
+
+    REXML::XPath.each(new_rates, "[@time='#{date.xmlschema}']") do |rate|
       return rate.children.reduce({}){ |cumulate, entry|
         key = entry.attribute('currency').value
         value = entry.attribute('rate').value
@@ -28,5 +51,29 @@ class Quote
         cumulate
       }
     end
+  end
+
+  def get_updated_rates
+  end
+
+  def using_default_from?
+    from == DEFAULT_FROM
+  end
+
+  def ensure_weekday date
+    # Make sure any dates are rounded
+    # back to the nearest last weekday 
+    # (as the ECB feed does not have rates
+    # mapped to weekend dates
+    Helper::ensure_weekday(date)
+  end
+
+  def date_in_range?(date = @date)
+    oldest_date = rates.last[:date]
+
+    return date if date >= oldest_date
+
+    raise Exception, 'Date too old - must be within last 3 months'
+    false
   end
 end
